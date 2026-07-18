f8d7e4af12ff66bf7681f675288f3eb48e6bac61
Ibrahima NDIAYE <ibrahima@kpsy.sn>
2026-07-18 22:53:10 +0000

    feat(security): intègration et configuration de helmet pour la sécurisation des en-têtes HTTP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(helmet({
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+  }));
 
   // Servir statiquement le dossier des uploads (ex: logos des tenants)
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
